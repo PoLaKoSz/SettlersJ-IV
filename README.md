1a2e890785b4e4119e022589f08d108a5cfe323b
Tom PoLáKoSz <polakosz@freemail.hu>
2020-02-04 11:50:29 +0100

    Add initial empty Console project

---
+++ README.md
@@ -0,0 +1,7 @@
+# SettlersJ IV.
+
+This is a Settlers IV. clone written in Java.
+
+## Requirements
+
+- Java JDK 11
---
+++ src/hu/polakosz/settlersj/iv/Main.java
@@ -0,0 +1,8 @@
+package hu.polakosz.settlersj.iv;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
